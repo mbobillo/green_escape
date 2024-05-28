a0bd33c54bd4c8f54f39f0eaadb02ec6599e4250
Kenzo Defoy <kenzo.defoy@icloud.com>
2024-05-28 12:44:15 +0200

    Corrected again the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,56 +24,6 @@ accomodations = Accomodation.create([
     title: 'Cozy Cabin',
     description: 'A small, cozy cabin in the woods.',
     price: 100.00,
-    images: 'cabin.jpg',
-    localisation: 'Forest',
-    environment_tag: 'countryside',
-    capacity: 4,
-    user: users.first
-  },
-  {
-    title: 'Beach House',
-    description: 'A beautiful house by the beach.',
-    price: 200.00,
-    images: 'beach_house.jpg',
-    localisation: 'Beach',
-    environment_tag: 'seaside',
-    capacity: 8,
-    user: users.last
-  }
-])
-
-bookings = Booking.create([
-  {
-    start_date: '2024-06-01',
-    end_date: '2024-06-07',
-    accomodation: accomodations.first,
-    user: users.first,
-  },
-  {
-    start_date: '2024-07-10',
-    end_date: '2024-07-15',
-    accomodation: accomodations.last,
-    user: users.last,
-  }
-])
-
-
-User.destroy_all
-Accomodation.destroy_all
-Booking.destroy_all
-
-users = User.create!([
-  { first_name: 'Marion', last_name: 'Bobillo', email: 'marion.bobillo@gmail.com', password: '123456' },
-  { first_name: 'Amelie', last_name: 'Caron', email: 'caron.amelie94@gmail.com', password: '123456' },
-  { first_name: 'Edgar', last_name: 'Meier', email: 'edgar.meier@swordagency.com', password: '123456' },
-  { first_name: 'Kenzo', last_name: 'Defoy', email: 'kenzo.defoy@gmail.com', password: '123456' }
-])
-
-accomodations = Accomodation.create([
-  {
-    title: 'Cozy Cabin',
-    description: 'A small, cozy cabin in the woods.',
-    price: 100.00,
     images: 'https://unsplash.com/photos/view-photography-of-triangular-house-in-forest-eMNRPvvNcxM',
     localisation: 'Forest',
     environment_tag: 'countryside',
