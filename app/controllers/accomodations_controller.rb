01ea0a1a1c6ab84cc6eafa0c4ed540663ec07706
Amelie Caron <caron.amelie94@gmail.com>
2024-05-28 11:35:52 +0200

    controller accomodation and views

--- app/controllers/accomodations_controller.rb
+++ app/controllers/accomodations_controller.rb
@@ -1,7 +1,30 @@
 class AccomodationsController < ApplicationController
   def index
-    @accomodation = Accomodation.all
+    @accomodations = Accomodation.all
   end
-    
+
+  def new
+    @accomodation = Accomodation.new
   end
-end
+
+  def show
+    @accomodation = Accomodation.find(params[:id])
+  end
+
+  def create
+    @accomodation = Accomodation.new(accomodation_params)
+    if @accomodation.save
+      redirect_to accomodations_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+  end
+
+  private
+  
+  def accomodation_params
+    params(:accomodation).permit(:title, :description, :price, :localisation, :images, :environment_tag, :capacity, :category, :user_id)
+  end
+
+  end
